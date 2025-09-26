653780fcc791f5b45b073aabd867cc76642c36a2
ViaYuSecE <via_alexa_yu@yahoo.com>
2025-09-26 09:10:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,33 @@ app.get("/", (req, res, next) => {
     res.send("Hello I am Via!!");
 });
 
+//Get all invoice
+app.get("/invoice", async (req, res, next) => {
+    const branch = req.query.branch;
+});
+
+//Get an invoice
+app.get("/invoice/:id", async (req, res, next) => {
+});
+
+//Add new invoice
+app.post("/invoice", async (req, res, next) => {
+});
+
+//Update an invoice
+app.put("/invoice/:id", async (req, res, next) => {
+});
+
+//Delete an invoice
+app.delete("/invoice/:id", async (req, res, next) => {
+});
+
+//Get summary
+app.get("/summary", async (req, res, next) => {
+    const branch = req.query.branch;
+});
+
+//Start server
 const http = require("http");
 
 app.listen(port, hostname, () => {
